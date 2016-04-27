92299a9b024a53e7b8fc8ac08a7c006622aced17
Kenneth Jiang <kenneth@rhumbix.com>
2016-04-27 10:54:54 -0700

    0.2.0. remove redudancy to call it

--- file-download.js
+++ file-download.js
@@ -1,10 +1,10 @@
 var React = require('react');
 
-module.exports.fileDownload = function(data, filename) {
-        var data = new Blob([data], {type: 'text/csv'});
-        var csvURL = window.URL.createObjectURL(data);
-        var tempLink = document.createElement('a');
-        tempLink.href = csvURL;
-        tempLink.setAttribute('download', filename);
-        tempLink.click();
+module.exports = function(data, filename) {
+    var data = new Blob([data], {type: 'text/csv'});
+    var csvURL = window.URL.createObjectURL(data);
+    var tempLink = document.createElement('a');
+    tempLink.href = csvURL;
+    tempLink.setAttribute('download', filename);
+    tempLink.click();
 }
