4dc24e9e981e450d00bc9366bf58d24cf6571e16
Kenneth Jiang <kenneth.jiang@gmail.com>
2016-06-28 19:00:57 +0000

    format

--- file-download.js
+++ file-download.js
@@ -1,10 +1,15 @@
 var React = require('react');
 
 module.exports = function(data, filename) {
-    var data = new Blob([data], {type: 'text/csv'});
-    var csvURL = window.URL.createObjectURL(data);
-    var tempLink = document.createElement('a');
-    tempLink.href = csvURL;
-    tempLink.setAttribute('download', filename);
-    tempLink.click();
+    var blob = new Blob([data], {type: 'text/csv'});
+    if (typeof window.navigator.msSaveBlob !== 'undefined') {
+            // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+            window.navigator.msSaveBlob(blob, filename);
+        } else {
+        var csvURL = window.URL.createObjectURL(blob);
+        var tempLink = document.createElement('a');
+        tempLink.href = csvURL;
+        tempLink.setAttribute('download', filename);
+        tempLink.click();
+    }
 }
