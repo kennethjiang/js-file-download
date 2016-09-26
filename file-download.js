28820212e25abb0fdf1e8117b24ca5d0689e2123
James Finucane <james@mpharma.co>
2016-09-26 15:54:20 +0000

    Fix for Firefox

--- file-download.js
+++ file-download.js
@@ -3,14 +3,19 @@ var React = require('react');
 module.exports = function(data, filename) {
     var blob = new Blob([data], {type: 'text/csv'});
     if (typeof window.navigator.msSaveBlob !== 'undefined') {
-            // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-            window.navigator.msSaveBlob(blob, filename);
-        } else {
+        // IE workaround for "HTML7007: One or more blob URLs were 
+        // revoked by closing the blob for which they were created. 
+        // These URLs will no longer resolve as the data backing 
+        // the URL has been freed."
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else {
         var csvURL = window.URL.createObjectURL(blob);
         var tempLink = document.createElement('a');
         tempLink.href = csvURL;
         tempLink.setAttribute('download', filename);
-        tempLink.setAttribute('target', '_blank');
+        document.body.appendChild(tempLink);
         tempLink.click();
+        document.body.removeChild(tempLink);
     }
 }
