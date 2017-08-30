1b237e7711b61efde03211483d50f850804e7722
Mike Spitz <MikeSpitz@users.noreply.github.com>
2017-08-30 13:54:45 +0100

    Add workaround to allow users on desktop safari to download files even if pop up blocking is enabled.
    NB: Safari on mobile does not support the download attribute and so you will need to have the "block pop ups" setting disabled in Settings -> Safari if attempting to download files on mobile devices. This is because safari on mobile does not have access to the file storage and thus cannot download things directly to the device.

--- file-download.js
+++ file-download.js
@@ -12,8 +12,16 @@ module.exports = function(data, filename, mime) {
         var tempLink = document.createElement('a');
         tempLink.style.display = 'none';
         tempLink.href = blobURL;
-        tempLink.setAttribute('download', filename);
-        tempLink.setAttribute('target', '_blank');
+        tempLink.setAttribute('download', filename); 
+        
+        // Safari thinks _blank anchor are pop ups. We only want to set _blank
+        // target if the browser does not support the HTML5 download attribute.
+        // This allows you to download files in desktop safari if pop up blocking 
+        // is enabled.
+        if (typeof tempLink.download === 'undefined') {
+            tempLink.setAttribute('target', '_blank');
+        }
+        
         document.body.appendChild(tempLink);
         tempLink.click();
         document.body.removeChild(tempLink);
