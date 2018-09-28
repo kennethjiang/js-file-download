ea29862741653a83a9a6d1b117e9622d25f61002
Andrew Banyard <andrew.banyard@civica.co.uk>
2018-09-28 09:25:12 +0100

    Add mime to type definition

--- js-file-download.d.ts
+++ js-file-download.d.ts
@@ -1,5 +1,5 @@
 declare module "js-file-download" {
 
-    export default function fileDownload(data: string, filename: string): void;
+    export default function fileDownload(data: string, filename: string, mime?:string): void;
     
 }
