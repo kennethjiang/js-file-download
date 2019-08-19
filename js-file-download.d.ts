f513d35d06b32a9f401ccb95b0cc52f848143498
Yuya Tanaka <yuya.presto@gmail.com>
2019-08-19 20:28:36 +0900

    Fix missing bom arg in d.ts

--- js-file-download.d.ts
+++ js-file-download.d.ts
@@ -2,6 +2,7 @@ declare module 'js-file-download' {
     export default function fileDownload(
         data: string | ArrayBuffer | ArrayBufferView | Blob,
         filename: string,
-        mime?: string
+        mime?: string,
+        bom?: string
     ): void;
 }
